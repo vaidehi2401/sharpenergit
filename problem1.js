a6de151bcb1680848968f1be23ee920b9fb66cfa
vaidehi2401 <dholevaidehi2@gmail.com>
2025-01-14 14:09:06 +0530

    added problem1

---
+++ problem1.js
@@ -0,0 +1,10 @@
+const heading = document.getElementById('main-heading')
+heading.innerHTML = 'Fruit World'
+heading.style.color = 'orange'
+const header = document.getElementById('header')
+header.style.backgroundColor = 'green'
+header.style.borderBottom = '2px solid orange'
+const fruitHeading = document.getElementById('basket-heading')
+fruitHeading.style.color='green'
+const thanks = document.getElementById('thanks')
+thanks.innerHTML = "<p>Please visit us again</p>"
